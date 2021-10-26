2691d13a4dce374e9f9987579476adb5bff50992
Zach <z.jessen@gmail.com>
2021-10-26 17:42:23 -0500

    bug fix

--- fastCorrelationMap.m
+++ fastCorrelationMap.m
@@ -1,5 +1,5 @@
 function map = fastCorrelationMap(image, sigma)
-c_image = image - mean(image,3);
+c_image = image - nanmean(image,3);
 
 map = sum(imgaussfilt(c_image, sigma).^2, 3)./ imgaussfilt(sum(c_image.^2,3), sigma);
 end
