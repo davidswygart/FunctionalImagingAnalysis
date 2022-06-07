a5e3e92711f5fa5d75163af40d6aaed93f2af3d8
davidswygart <davidswygart2015@u.northwestern.edu>
2022-06-07 17:08:52 -0500

    Update AnalyzeUV.m

--- david/AnalyzeUV.m
+++ david/AnalyzeUV.m
@@ -1,16 +1,22 @@
-gChannel = 2;
-image = 'NoCube_00004.tif';
+function res = AnalyzeUV()
+path = 'D:\Images\060622B_PMT_filterTest';
+image = 'doubleGreenBandAndDavidUV_00001';
 
-
-%% get raw image (and time image)
-
-
-path = 'C:\Users\david\Desktop\UV_test';
 stimChannel = 4;
 start = -1;
-stop = 1;
-[raw,metaData] = getRaw([path filesep image]);
-eImg = splitRawImageIntoEpochs2(raw(:,:,:,stimChannel), raw(:,:,:,gChannel), metaData, [start,stop]);
-eResp = epochPixelResponse(eImg.green, eImg.time);
+stop = 2;
+%% get raw image (and time image)
+[raw,metaData] = getRaw([path filesep image '.tif']);
+eImgCh1 = splitRawImageIntoEpochs2(raw(:,:,:,stimChannel), raw(:,:,:,1), metaData, [start,stop]);
+eImgCh2 = splitRawImageIntoEpochs2(raw(:,:,:,stimChannel), raw(:,:,:,2), metaData, [start,stop]);
 
-mean(eResp.stim(:)) - mean(eResp.pre(:))
+eRespCh1 = epochPixelResponse(eImgCh1.green, eImgCh1.time);
+eRespCh2 = epochPixelResponse(eImgCh2.green, eImgCh2.time);
+
+avgCh1 = squeeze(mean(eRespCh1.stim, [1,2]) - mean(eRespCh1.pre, [1,2]));
+avgCh2 = squeeze(mean(eRespCh2.stim, [1,2]) - mean(eRespCh2.pre, [1,2]));
+
+res = struct;
+res.avg = [mean(avgCh1) mean(avgCh2)];
+res.std = [std(avgCh1) std(avgCh2)];
+end
